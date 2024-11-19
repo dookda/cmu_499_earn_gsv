b2689284e8496d6a5bb90556c7a84bd3f20fa1b0
dookda <sakda.homhuan@gmail.com>
2024-11-19 08:30:20 +0700

    update

--- python/gsv_main3.py
+++ python/gsv_main3.py
@@ -6,7 +6,7 @@ import scipy.misc
 from io import BytesIO
 from PIL import Image
 import matplotlib.pyplot as plt
-import cv2
+from cv2 import cv2
 import key
 import torch
 from ultralytics import YOLO
